5e4fd862ffcd86faf54fff639537e8ac464bd7bb
Kisik Chang <honors@paran.com>
2025-12-31 12:40:35 +0000

    10장 업데이트

--- chapter10/airflow/dags/recurring_player_api_insert_update_dag.py
+++ chapter10/airflow/dags/recurring_player_api_insert_update_dag.py
@@ -37,7 +37,19 @@ def recurring_player_api_insert_update_dag():
         response_check=health_check_response,
     )
 
-    temp_min_last_change_date = Variable.get("temp_min_last_change_date")
+    # Airflow WEB UI에서 아래 에러 발생 시 최소 변경 날짜 변수를 추가해야 함
+    #
+    # DAG Import Errors (1)expand_less expand_moreBroken DAG: 
+    # [/opt/airflow/dags/recurring_player_api_insert_update_dag.py] Traceback (most recent call last): 
+    # File "/opt/airflow/dags/recurring_player_api_insert_update_dag.py", line 40, 
+    # in recurring_player_api_insert_update_dag temp_min_last_change_date = Variable.get("temp_min_last_change_date")
+    #  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+    # File "/home/airflow/.local/lib/python3.12/site-packages/airflow/models/variable.py", line 145, 
+    # in get raise KeyError(f"Variable {key} does not exist") KeyError: 'Variable temp_min_last_change_date does not exist'
+    # 
+    # 
+    # temp_min_last_change_date = Variable.get("temp_min_last_change_date")
+    temp_min_last_change_date = Variable.get("temp_min_last_change_date", default_var="2024-01-01")
 
     api_player_query_task = HttpOperator(
         task_id="api_player_query",
